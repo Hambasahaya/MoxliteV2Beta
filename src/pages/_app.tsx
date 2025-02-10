1941223ffaedb0be8459a5ad66c057f13caedda6
Timotius Nugroho <timotius.nugroho@bni.co.id>
2025-02-11 00:24:55 +0700

    setup css & font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Inter, Saira } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const saira = Saira({ subsets: ["latin"], variable: "--font-saira" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${inter.variable} ${saira.variable}`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
